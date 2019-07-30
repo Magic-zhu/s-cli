aae5f63867d7ebfbedff66e78b0b2313e7d54d07
alex.zhu <2489180202@qq.com>
2019-07-30 13:19:40 +0800

    update:日常更新

--- command/init.js
+++ command/init.js
@@ -7,17 +7,46 @@ module.exports = function(){
         {
             type:'list',
             name:'template',
-            message:"请选择需要克隆的模板?",
+            message:"请选择需要克隆的基础模板?",
             choices: [
-                "vue",
-                "vue+vuex",
-                "react",
+                "Vue",
+                "Vue+Vuex",
+                "React",
+                "React+Redux"
             ]
         }
     ]).then(answers=>{
-        console.log(answers)
-        inquirer.prompt([
+        console.log(answers.template)
+        let questions = [];
+        switch(answers.template){
+            case "Vue":
+                questions = [
+                    {
+                        type:'list',
+                        name:'css',
+                        message:"请选择css预处理器?",
+                        choices: [
+                            "less",
+                            "scss",
+                            "stylus",
+                        ]
+                    },
+                    {
+                        type:'list',
+                        name:"ui",
+                        message:"请选择UI库",
+                        choices:[
+                            "element-ui",
+                            'mint-ui',
+                            "cube-ui",
+                            "不使用"
+                        ]
+                    }
+                ];
+                break
+        }
+        inquirer.prompt(questions).then(answers=>{
 
-        ])
+        })
     })
 }
---
+++ command/plugin.js
@@ -0,0 +1,6 @@
+var child = require('child_process');
+var chalk = require('chalk');
+var inquirer = require('inquirer');
+module.exports = ()=>{
+    
+}
