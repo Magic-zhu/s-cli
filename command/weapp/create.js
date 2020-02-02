73cae40775ce5e15617276ca82d593232313cd30
magiczhu <18817313320@163.com>
2020-02-02 21:10:22 +0800

    update

--- command/weapp/create.js
+++ command/weapp/create.js
@@ -5,6 +5,18 @@ const spinner = require('ora')();
 const Rx = require('rxjs/Rx');
 const Writer = Rx.Observable.bindNodeCallback(fs.writeFile);
 const Reader = Rx.Observable.bindNodeCallback(fs.readFile);
-module.exports = function (option) {
-    
+
+module.exports = function (name,e) {
+
+    let generate_path = e.path?e.path:'./'
+    generate_path = path.resolve(generate_path)
+
+    if(e.component){
+        console.log(path)
+    }
+
+    if(e.page){
+
+    }
+
 }
--- command/weapp/init.js
+++ command/weapp/init.js
@@ -1,8 +1,11 @@
 const path = require('path')
-const chalk = require('chalk')
-const inquirer = require('inquirer')
+const child = require('child_process')
 const spinner = require('ora')()
-import loadTemplate from "../../utils/loadTemplate"
-module.exports = function () {
-
+const Rx = require('rxjs/Rx')
+const Shell = Rx.Observable.bindNodeCallback(child.exec);
+module.exports = function (name) {
+    spinner.start()
+    Shell(`git clone https://gitee.com/magic-zhu/template-weapp.git ${name}`).subscribe(res=>{
+        spinner.succeed('初始化成功')
+    })
 }
