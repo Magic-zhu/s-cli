07c2fb040dc0f4e20df2ee2faabad0d084a44893
magiczhu <18817313320@163.com>
2020-02-12 18:49:56 +0800

    update

--- ui/views/weapp/index.jsx
+++ ui/views/weapp/index.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
-import { Button, List, Typography, Input, Switch } from 'antd';
+import { Button, List, Typography, Input, Switch, Anchor, Icon, Radio } from 'antd';
+const { Link } = Anchor;
 import './index.less'
 export default class WeApp extends Component {
     constructor(props) {
         super(props)
         this.state = {
             type: 0,
-            pages: ['/page/index/index']
+            pages: ['/page/index/index'],
+            config: {
+
+            },
+            float_menu: [
+                { name: 'pages' },
+                { name: 'window' },
+                { name: 'tabBar' },
+                { name: 'networkTimeout' },
+                { name: 'debug' },
+                { name: 'funtionalPages' },
+                { name: 'subpackages' },
+                { name: 'workers' },
+                { name: 'requiredBackgroundModes' },
+                { name: 'plugins' },
+                { name: 'preloadRule' },
+                { name: 'resizable' },
+                { name: 'navigateToMiniProgramAppidList' },
+                { name: 'usingComponents' },
+                { name: 'permission' },
+                { name: 'sitemapLocation' },
+                { name: 'style' },
+                { name: 'useExtendedLib' },
+            ],
         }
     }
     render() {
@@ -27,7 +51,7 @@ export default class WeApp extends Component {
                     </div>
                 </div>
                 <div className='rightside'>
-                    <header>配置表</header>
+                    <header>配置表 </header>
                     <article>
                         <List
                             header={<div><h1>pages</h1>用于指定小程序由哪些页面组成，每一项都对应一个页面的 路径（含文件名） 信息。</div>}
@@ -45,14 +69,36 @@ export default class WeApp extends Component {
                             )}
                         />
                         <div className="windows">
-                            <div>
-                                <span>navigationBarBackgroundColor:导航栏背景颜色，如 #000000</span>
+                            <h1 name='window'>window</h1>
+                            <div className='windows_item'>
+                                <div><Icon type="edit" theme="filled" /> navigationBarBackgroundColor:导航栏背景颜色，如 #000000</div>
                                 <div>
                                     <Input placeholder="#000000" className='w200' />
                                 </div>
                             </div>
+                            <div className='windows_item'>
+                                <div><Icon type="edit" theme="filled" /> 导航栏标题颜色</div>
+                                <div>
+                                    <Radio.Group >
+                                        <Radio value={'black'}>black</Radio>
+                                        <Radio value={'white'}>white</Radio>
+                                    </Radio.Group>
+                                </div>
+                            </div>
                         </div>
                     </article>
+                </div>
+                <div className='float_menu'>
+                    <Anchor>
+                        {this.state.float_menu.map((item) => {
+                            return (
+                                <Link
+                                    href={'#' + item.name}
+                                    title={item.name}
+                                />
+                            )
+                        })}
+                    </Anchor>
                 </div>
             </div>
         )
