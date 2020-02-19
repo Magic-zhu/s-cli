72241b576d22ae92537ca642336feb8a311314e2
magic-zhu <18817313320@163.com>
2020-02-19 23:06:39 +0800

    更新系统树组件

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const file = require("../functions/file")
+
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post("/getFileTree",function(req, res, next){
+  let {body} = req;
+  let tree = file.readDir(body.path)
+  res.json(tree)
+})
 
 module.exports = router;
--- ui/components/FileSystem/index.jsx
+++ ui/components/FileSystem/index.jsx
@@ -1,43 +1,87 @@
-import React ,{ Component } from "react";
+import React, { Component } from "react";
 import "./index.less";
-import io from "socket.io-client";
-
-const socket = io("http://47.105.210.34:8086/", {
-    transports: ["websocket", "xhr-polling", "jsonp-polling"],
-    autoConnect: false
-}); //顺带解决本地的跨域
-
+import axios from "axios"
+import { Icon ,Button} from "antd"
 export default class FileSystem extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
-            fileList:[],
+        this.state = {
+            fileList: [],
+            nowPath: "",
+            pathArray:[],//路径数组
         };
+        this.chooseFolder.bind(this);
+        this.back.bind(this);
     }
     componentDidMount() {
-        console.log(1)
-        socket.on('connect',()=>{
-            console.log('对接ok')
-        })
-        socket.on('_getFileTree',data=>{
-            this.setState({
-                fileList:data
-            })
-        })
-        socket.on('disconnect', function(err){
-            console.log(222)
-        });
-        socket.emit('getFileTree')
+        this.getFileTree("/")
     }
-
+    getFileTree(path) {
+        axios.post('http://localhost:4066/getFileTree', { path }).then(res => {
+            this.setState({
+                fileList: res.data,
+                nowPath:path==="/"?"":path,
+            });
+        })
+    }
+    chooseFolder(dir,name) {
+        if(!dir){return}
+        let path = this.state.nowPath + "/" + name;
+        let temp = [];
+        temp = temp.concat(this.state.pathArray);
+        temp.push("/" + name);
+        this.setState({
+            pathArray:temp
+        })
+        this.getFileTree(path);
+    }
+    back(){
+        let t = this.state.pathArray.concat([]);
+        if(t.length<=1){
+            this.setState({
+                pathArray : [],
+                nowPath:"",
+            })
+            this.getFileTree("/")
+        }
+        t.pop();
+        let path = "";
+        t.forEach(i=>{
+            path = path + i
+        })
+        this.setState({
+            pathArray : t,
+            nowPath:path,
+        })
+        this.getFileTree(path)
+    }
     render() {
         return (
             <div className={'FileSystem'}>
-                {this.state.fileList.map((item,index)=>{
-                    return (
-                        <div key={index}>{item.name}</div>
+                <div className="nowPath">当前路径: 
+                <span>根目录</span> 
+                {this.state.pathArray.map(item=>{
+                    return(
+                        <span>{item}</span>
                     )
-                })}
+                })}</div>
+                <div className="content">
+                    {this.state.fileList.map((item, index) => {
+                        return (
+                            <div
+                                key={index} className='item'
+                                onClick={() => { this.chooseFolder(item.dir,item.name) }}
+                            >
+                                {item.dir ? <Icon type="folder" theme="twoTone" /> : <Icon type="file" />}
+                                {item.name}
+                            </div>
+                        )
+                    })}
+                </div>
+                <div>
+                    <Button type='default' onClick={()=>{this.back()}}>返回</Button>
+                    <Button type='primary'>选择</Button>
+                </div>
             </div>
         )
     }
