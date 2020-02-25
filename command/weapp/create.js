8216d41d3f258693de82a065d97aa792c8507d8c
magic-zhu <18817313320@163.com>
2020-02-25 22:29:40 +0800

    update

--- command/weapp/create.js
+++ command/weapp/create.js
@@ -5,30 +5,43 @@ const Rx = require('rxjs/Rx');
 const Writer = Rx.Observable.bindNodeCallback(fs.writeFile);
 const Reader = Rx.Observable.bindNodeCallback(fs.readFile);
 
+function registerRoute(name){
+    message.info("读取app.json文件");
+    Reader(path.resolve("./app.json"))
+        .toPromise()
+        .then(buffer => {
+            let config = JSON.parse(buffer.toString())
+            config.pages.push(`pages/${name}/index`)
+            return Writer(path.resolve("./app.json"), JSON.stringify(config)).toPromise()
+        })
+        .then(res => {
+            message.success('路由添加成功')
+        })
+        .catch(err => {
+            message.error(err)
+        })    
+}
+
+function createNewPage(name){
+
+}
+
+function createNewComponent(){
+
+}
+
 module.exports = function (name, e) {
 
     let generate_path = e.path ? e.path : './'
     generate_path = path.resolve(generate_path)
 
     if (e.component) {
-        console.log(path)
+        
     }
 
     if (e.page) {
-        message.info("读取app.json文件");
-        Reader(path.resolve("./app.json"))
-            .toPromise()
-            .then(buffer => {
-                let config = JSON.parse(buffer.toString())
-                config.pages.push(`pages/${name}/index`)
-                return Writer(path.resolve("./app.json"), JSON.stringify(config)).toPromise()
-            })
-            .then(res => {
-                message.success('路由添加成功')
-            })
-            .catch(err => {
-                message.error(err)
-            })
+
+        registerRoute(name,e.path)
     }
 
 }
--- command/weapp/init.js
+++ command/weapp/init.js
@@ -1,11 +1,12 @@
-const path = require('path')
 const child = require('child_process')
-const spinner = require('ora')()
+const message = require("../../utils/message")
 const Rx = require('rxjs/Rx')
 const Shell = Rx.Observable.bindNodeCallback(child.exec);
 module.exports = function (name) {
-    spinner.start()
-    Shell(`git clone https://gitee.com/magic-zhu/template-weapp.git ${name}`).subscribe(res=>{
-        spinner.succeed('初始化成功')
+    message.info('初始化项目中...')
+    let sh = `git clone https://gitee.com/magic-zhu/template-weapp.git ${name}`
+    Shell(sh)
+    .subscribe(()=>{
+        message.success('初始化成功')
     })
 }
