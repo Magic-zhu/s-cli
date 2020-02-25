ca104ba0d40aa3353b2de75a226cbb7cc2077bf8
alex.zhu <2489180202@qq.com>
2020-02-25 18:41:45 +0800

    更新

--- command/weapp/create.js
+++ command/weapp/create.js
@@ -1,22 +1,34 @@
 const path = require('path');
 const fs = require('fs');
 const message = require('../../utils/message');
-const spinner = require('ora')();
 const Rx = require('rxjs/Rx');
 const Writer = Rx.Observable.bindNodeCallback(fs.writeFile);
 const Reader = Rx.Observable.bindNodeCallback(fs.readFile);
 
-module.exports = function (name,e) {
+module.exports = function (name, e) {
 
-    let generate_path = e.path?e.path:'./'
+    let generate_path = e.path ? e.path : './'
     generate_path = path.resolve(generate_path)
 
-    if(e.component){
+    if (e.component) {
         console.log(path)
     }
 
-    if(e.page){
-
+    if (e.page) {
+        message.info("读取app.json文件");
+        Reader(path.resolve("./app.json"))
+            .toPromise()
+            .then(buffer => {
+                let config = JSON.parse(buffer.toString())
+                config.pages.push(`pages/${name}/index`)
+                return Writer(path.resolve("./app.json"), JSON.stringify(config)).toPromise()
+            })
+            .then(res => {
+                message.success('路由添加成功')
+            })
+            .catch(err => {
+                message.error(err)
+            })
     }
 
 }
