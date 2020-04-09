95b994fcdbe773ef70c0c72b1c359d937166c224
magic-zhu <18817313320@163.com>
2020-04-09 17:50:48 +0800

    存档

--- command/scan.js
+++ command/scan.js
@@ -6,5 +6,11 @@ const Writer = Rx.Observable.bindNodeCallback(fs.writeFile);
 const Reader = Rx.Observable.bindNodeCallback(fs.readFile);
 const message = require('../utils/message');
 module.exports=function(){
-    message.info('啊啊啊')
+    Reader('/Users/alex.zhu/Desktop/MCNweapp1912/pages/my_task/index.wxml').subscribe(buffer=>{
+        let string = buffer.toString();
+        string = string.replace(/[\t\n]/g,"");
+        const startTagOpen = /^<[a-zA-Z_-]+/;
+        let res = string.match('true');
+        console.log(res[0],res.index)
+    })
 }
